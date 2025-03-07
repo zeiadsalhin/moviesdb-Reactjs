f60b3c9e8423c17acbf4eac90be5a831dd3041c5
ZiadATawab <99220048+zeiadsalhin@users.noreply.github.com>
2025-03-07 09:57:51 +0200

    Update DetailsPage.jsx

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -114,7 +114,7 @@ const DetailsPage = () => {
               
               {data.popularity && (
                 <Grid item>
-                  <Typography variant="body1">{(data.popularity ? data.popularity.toFixed(0) : 0).toLocaleString()} Reviews</Typography>
+                  <Typography variant="body1">{Number(data.popularity?.toFixed()).toLocaleString()} Reviews</Typography>
                 </Grid>
               )}
               
